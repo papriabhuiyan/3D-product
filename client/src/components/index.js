665e2039d3c87687f519a928f7f344bd3c1c3b35
papriabhuiyan <pbhuiyan@smu.edu>
2023-06-13 12:12:25 -0500

    added more components for Customization page, started working on the side bar

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,20 @@
 import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
-export { CustomButton }
+
+export {
+    AIPicker,
+    ColorPicker,
+    CustomButton,
+    FilePicker,
+    Tab
+}
+
+
+
+
+
+
