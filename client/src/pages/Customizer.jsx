5faa02cd5e72d5f85c12aebcc4b4dd6108cea251
papriabhuiyan <56096609+papriabhuiyan@users.noreply.github.com>
2023-06-13 17:18:08 -0500

    added button to route to homepage, added customization tabs with animations

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -22,9 +22,39 @@ const Customizer = () => {
           >
             <div className='flex items-center min-h-screen'>
               <div className='editortabs-container tabs'>
-                
+                {EditorTabs.map((tab) => (
+                  <Tab
+                    key={tab.name}
+                    tab = {tab}
+                    handleClick ={() => {}}
+                  />
+                ))}
               </div>
             </div>
+          </motion.div>
+          <motion.div
+            className='absolute z-10 top-5 right-5'
+            {...fadeAnimation}
+          >
+            <CustomButton
+              title="Go Back"
+              type="filled"
+              handleClick={()=>{state.intro=true}}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            >
+            </CustomButton>
+          </motion.div>
+          <motion.div
+            className="filtertabs-container"
+            {...slideAnimation('up')}
+          >
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab = {tab}
+                handleClick ={() => {}}
+              />
+            ))}
           </motion.div>
         </>
       )}
