7dad5ccf3af777ac05959cdd6b652937fef17f6d
papriabhuiyan <pbhuiyan@smu.edu>
2023-06-12 23:57:50 -0500

    github push successful, deleted the test comments and code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,8 +4,6 @@ import { useSnapshot } from 'valtio'
 import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from '../config/motion'
 import state from '../store'
 
-
-//added github publish test comment
 const Home = () => {
     const snap = useSnapshot(state);
   return (
@@ -14,9 +12,6 @@ const Home = () => {
             <motion.section className='home' {...slideAnimation('left')}>
                 <motion.header {...slideAnimation('down')}>
                     <img src='./threejs.png' alt='logo' className='w-8 h-8 object-contain'/>
-                </motion.header>
-                <motion.header>
-                    <p> Testing github publish from work computer to work on this at work during work hours</p>
                 </motion.header>
             </motion.section>
         )}
