948bca806f0295ac19776e69438243f8b28bfc65
papriabhuiyan <56096609+papriabhuiyan@users.noreply.github.com>
2023-06-19 18:55:38 -0500

    added the icons to the tabs

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -11,7 +11,7 @@ const CanvasModel = () => {
   return (
     <Canvas
       shadows
-      camera={{position: [0, 0, 0], fov: 35}}
+      camera={{position: [0, 0, 0], fov: 25}}
       gl={{ preserveDrawingBuffer: true }}
       className='w-full max-w-full h-full transition-all ease-in'
     >
